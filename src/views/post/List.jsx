d6c05ceb7250a8596dd73d4ec994522c3839a957
nam20 <nam741585@naver.com>
2024-09-05 23:45:33 +0900

    feat: Data Grid sample page

--- src/views/post/List.jsx
+++ src/views/post/List.jsx
@@ -1,20 +1,18 @@
-// material-ui
-import Typography from '@mui/material/Typography';
+import { DataGrid } from '@mui/x-data-grid';
 
-// project imports
-import MainCard from 'ui-component/cards/MainCard';
+const rows = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+]
 
-// ==============================|| SAMPLE PAGE ||============================== //
+const columns = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
 
-const SamplePage = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
+const PostList = () => (
+  <DataGrid rows={rows} columns={columns} />
 );
 
-export default SamplePage;
+export default PostList;
