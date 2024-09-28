edff7306553ebefd12d08a514c320f89a063156d
nam20 <nam741585@naver.com>
2024-09-28 19:23:32 +0900

    feat: 로그인 여부 확인 API 호출 로직 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { ThemeProvider } from '@mui/material/styles';
@@ -12,11 +12,32 @@ import themes from 'themes';
 
 // project imports
 import NavigationScroll from 'layout/NavigationScroll';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { USER } from 'store/actionTypes';
 
 // ==============================|| APP ||============================== //
 
 const App = () => {
   const customization = useSelector((state) => state.customization);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const apiUrl = import.meta.env.VITE_APP_API_URL;
+
+      try {
+        const { data } = await axios.get(`${apiUrl}/api/auth/check`, { withCredentials: true });
+        if (data) {
+          dispatch({ type: USER, payload: data });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    checkAuth();
+  }, [dispatch]);
 
   return (
     <StyledEngineProvider injectFirst>
